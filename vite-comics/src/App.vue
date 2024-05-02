3c90e5b012c9af570c2882adcc430f162fd72a88
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-02 15:55:49 +0200

    feat: AppHeader done

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,13 +1,32 @@
 <script>
-export default{
+import AppHeader from "./components/AppHeader.vue";
 
+export default{
+    components: {
+      AppHeader
+    },
+    data(){
+      return{
+        
+      }
+    }
 }
 </script>
 
 
 <template>
-
+    <AppHeader/>
 </template>
 
 <style lang="scss">
+
+body {
+  background-color: lightcoral;
+  color: white;
+}
+
+.container {
+  width: 70%
+}
+
 </style>
---
+++ vite-comics/src/components/AppHeader.vue
@@ -0,0 +1,110 @@
+<script>
+export default {
+  data() {
+    return {
+      menuLinks: [
+        {
+          title: "characters",
+          isActive: true,
+        },
+        {
+          title: "comics",
+          isActive: false,
+        },
+        {
+          title: "movies",
+          isActive: false,
+        },
+        {
+          title: "TV",
+          isActive: false,
+        },
+        {
+          title: "games",
+          isActive: false,
+        },
+        {
+          title: "collectibles",
+          isActive: false,
+        },
+        {
+          title: "videos",
+          isActive: false,
+        },
+        {
+          title: "fans",
+          isActive: false,
+        },
+        {
+          title: "news",
+          isActive: false,
+        },
+        {
+          title: "shop",
+          isActive: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <img src="../assets/img/dc-logo.png" alt="" />
+      <ul>
+        <li v-for="link in menuLinks">
+          <a href="#" :class="{active: link.isActive}">
+            {{ link.title.toUpperCase() }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "./style/partials/variables" as *;
+@use "./style/partials/mixins" as *;
+
+header {
+  background-color: $header-color;
+}
+
+.container {
+  // Mixin Flex
+  @include flex(row, space-between, center);
+
+  margin: 0 auto;
+  padding: 1.5rem 0;
+
+  ul {
+    @include flex(row, space-between, center);
+    gap: 2rem;
+    list-style: none;
+    font-family: monospace;
+    font-weight: bold;
+    font-size: 1rem;
+
+    li{
+        a{
+            text-decoration: none;
+            color: $header-text-color;
+
+            &:hover{
+                color: $primary-color;
+            }
+
+            &.active{
+                border-bottom: 4px;
+                border-color: $primary-color; 
+            }
+        }
+
+
+    }
+
+  }
+}
+</style>
